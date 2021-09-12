7fa3edd6a1bc760d3cd212adcc298e51f031ec0f
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-09-12 19:35:02 -0400

    json encoded policy

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "vault_backend" {
 resource "aws_iam_policy" "vault_backend" {
   name = "vault_backend_policy"
   description = "IAM policy allowing Vault to leverage an S3 backend"
-  policy = data.aws_iam_policy_document.vault_backend
+  policy = data.aws_iam_policy_document.vault_backend.json
 }
 
 resource "aws_iam_user" "vault_backend" {
